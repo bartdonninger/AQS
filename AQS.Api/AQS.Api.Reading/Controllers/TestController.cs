f04b44bf52c76f541ce079450a850e1d769681dc
Bart Donninger <donningerbart@gmail.com>
2020-07-30 13:38:32 +0200

    Add test controller and change DB connectionstring

---
+++ AQS.Api/AQS.Api.Reading/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AQS.Api.Reading.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get(string input)
+        {
+            return Ok($"Hoi {input}");
+        }
+    }
+}
