4cc2d35bfef01d0c98ddc7d27cee80443a6dcace
Woytek Bode <woytek.bode@fiberneering.com>
2020-07-30 17:23:04 +0200

    Animal testing

---
+++ AQS.Api/Animals/AnimalTypes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    interface IAnimal
+    {
+        string name_ { get; set; }
+        string Talk();
+    }
+
+    class Dog : IAnimal
+    {
+        public string name_ { get; set; }
+        public string bark_ { get; set; }
+        public string Talk()
+        {
+            return bark_;
+        }
+    }
+
+    class Cat : IAnimal
+    {
+        public string name_ { get; set; }
+        public string Talk()
+        {
+            return "Meow!";
+        }
+    }
+}
---
+++ AQS.Api/Animals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<IAnimal>();
+
+            animals.Add(new Dog { name_ = "Bart", bark_ = "Wooooof!" });
+            animals.Add(new Dog { name_ = "Woytek", bark_ = "Wroof!" });
+            animals.Add(new Cat { name_ = "Robin"});
+
+            PrintAnimals(animals);
+        }
+
+        private static void PrintAnimals(IEnumerable<IAnimal> animals)
+        {
+            foreach (IAnimal animal in animals)
+            {
+                Console.WriteLine("Here is {0}: {1}", animal.name_, animal.Talk());
+            }
+        }
+    }
+}
